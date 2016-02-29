f1ab76c5b18452ed6eed2135b9fef524498c599e
IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@hq-lap-2186.impello.co.uk>
2016-02-29 17:31:56 +0000

    Signed-off-by: IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@hq-lap-2186.impello.co.uk>

--- src/test/java/com/rupert/guildedrose/GildedRoseTest.java
+++ src/test/java/com/rupert/guildedrose/GildedRoseTest.java
@@ -5,16 +5,36 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
-
+	
     @Test
-    public void dexterityVestTest() {
-        Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20) };
+    public void randomNormalItemTestStillWithinSellByDate() {
+        Item[] items = new Item[] { new Item("Normal Item", 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("+5 Dexterity Vest", app.items[0].name);
+        assertEquals("Normal Item", app.items[0].name);
         assertEquals(9, app.items[0].sellIn);
         assertEquals(19, app.items[0].quality);		
     }
-   
+	
+    @Test
+    public void randomNormalItemTestPassedSellByDate() {
+        Item[] items = new Item[] { new Item("Normal Item", -5, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Normal Item", app.items[0].name);
+        assertEquals(-6, app.items[0].sellIn);
+        assertEquals(18, app.items[0].quality);		
+    }
+    
+    @Test
+    public void randomNormalItemTestQualityNotNegative() {
+        Item[] items = new Item[] { new Item("Normal Item", -5, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Normal Item", app.items[0].name);
+        assertEquals(-6, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);		
+    }
+  
     
 }
